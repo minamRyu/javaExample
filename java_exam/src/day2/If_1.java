4c9b12af4d02b7ab4d522bf75c75784077ccfdd6
tj-bu-708-PC03 <tj-bu-708-PC03@DESKTOP-55008N1>
2024-12-27 15:33:32 +0900

    first commit

---
+++ java_exam/src/day2/If_1.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class If_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 15;
+		// 단순  if
+//		if(num >= 20) {
+//			System.out.println("20보다 크거나 같다.");
+//		}
+		
+		// if ~ else
+		if(num >= 20) {
+			System.out.println("20보다 크거나 같다.");
+		} else {
+			System.out.println("20보다 작다.");
+		}
+		
+		
+
+	}
+
+}
---
+++ java_exam/src/day2/Ternary.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class Ternary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 3;
+//		if(num > 0) {
+//			System.out.println("양수");
+//		} else {
+//			System.out.println("음수");
+//		}
+		// 위의 코드를 삼항 연산자로 표현하면
+		
+		String result = num > 0 ? "양수" : "음수";
+		System.out.println(result);
+		
+
+	}
+
+}
