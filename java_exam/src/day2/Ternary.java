236f38f4a3013d609268b5629489953fc6fe4b0b
tj-bu-708-PC03 <tj-bu-708-PC03@DESKTOP-55008N1>
2024-12-27 15:55:44 +0900

    java20241227

--- java_exam/src/day2/Ternary.java
+++ java_exam/src/day2/Ternary.java
@@ -5,6 +5,7 @@ public class Ternary {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num = 3;
+		
 //		if(num > 0) {
 //			System.out.println("양수");
 //		} else {
