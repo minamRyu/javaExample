4f908b831348b213e6cee0aee926696707153f1d
tj-bu-708-PC03 <tj-bu-708-PC03@DESKTOP-55008N1>
2024-12-30 18:20:12 +0900

    Java_중첩반복_구구단_20241230

--- java_exam/src/day3/StringEx1.java
+++ java_exam/src/day3/StringEx1.java
@@ -8,7 +8,9 @@ public class StringEx1 {
 		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		String input = scan.next();
-		if(input.equals("종료")) {
+		// String은 객체이므로, 문자열 비교는 메소드를 사용해라.
+		// 숫자는 그냥 == 쓰면 됨
+		if(input.equals("종료")) { 
 			System.out.println("종료되었습니다.");
 		} else {
 			System.out.println("실행 중입니다.");
