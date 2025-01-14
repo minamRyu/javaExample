e0d37e072e6cb9cf838d08c4627fbf220ca27b7a
lenovo <lenovo@192.168.0.4>
2025-01-14 23:05:33 +0900

    20250114_ListMap_GUI

--- java_exam/src/day13/MyFrame3.java
+++ java_exam/src/day13/MyFrame3.java
@@ -4,31 +4,37 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MyFrame3 extends JFrame{
 	
 	public MyFrame3() {
 		setTitle("첫번째 GUI");
-		
+
 		Container c = getContentPane();
-		c.setBackground(Color.gray);
-		GridLayout grid = new GridLayout(3, 3);
-		
-		grid.setHgap(5);
+		c.setBackground(Color.cyan);
+		// GridLayout
+		GridLayout grid = new GridLayout(4, 3);
 		grid.setVgap(5);
-		
-		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 아래 3줄은 필수로 설정
+		grid.setHgap(5);
+		c.setLayout(grid);
+
+		for (int i = 1; i <= 9; i++) {
+			c.add(new JButton(i + ""));
+		}
+		c.add(new JButton("+"));
+		c.add(new JButton("0"));
+		c.add(new JButton("="));
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(300, 300);
 		setVisible(true);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		MyFrame3 gui = new MyFrame3();
-
 	}
 
 }
